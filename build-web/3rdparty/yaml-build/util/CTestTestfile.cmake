8e164fa2eacba65ceeaf6780c81dcd43ea6d3166
Alexei Rusu <alexei.r.2002@gmail.com>
2026-02-11 00:45:46 +0200

    cleanup: remove the build-web folder

--- build-web/3rdparty/yaml-build/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/toor/Code/toolbox/3rdparty/.deps/yaml-src/util
-# Build directory: /home/toor/Code/toolbox/build-web/3rdparty/yaml-build/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
